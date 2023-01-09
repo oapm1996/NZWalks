aa37e8a4a0e4c06c40f41687b9f69dd990e1fbf2
oapm1996 <isc.oscarpina@gmail.com>
2023-01-09 15:26:28 -0800

    until 22 video

--- NZWalks.API/NZWalks.API/Program.cs
+++ NZWalks.API/NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+//Injeccion de DBContext class en la colleccion de servicios
+builder.Services.AddDbContext<NZWalksDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
